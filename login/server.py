2a5f4ab9d8bf48ab6160681b32a040a7e50cc0f5
butter-cake <ernchae@gmail.com>
2023-06-27 06:25:49 -0700

    fifth

--- login/server.py
+++ login/server.py
@@ -1,5 +1,7 @@
 from flask_app import app
 from flask_app.controllers import users_controller
 
+# if __name__ == '__main__':
+#     app.run(debug=True, host='localhost', port=8000)
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=8000)
+    app.run(debug=False, host='0.0.0.0', port=8000)
